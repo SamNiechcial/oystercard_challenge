31d07112b248498df73aa3dd33e4ba39aa92733b
Sam Niechcial <samniechcial@gmail.com>
2018-03-28 14:37:19 +0100

    Complete challenge #12: Adding journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,8 @@
 class Oystercard
   attr_reader :balance
   attr_reader :entry_station
+  attr_reader :exit_station
+  attr_reader :journey_history
 
   DEFAULT_BALANCE = 0.0
   MAXIMUM_BALANCE = 90.0
@@ -11,12 +13,12 @@ class Oystercard
     max_bal_err = "Cannot create card with more than £#{MAXIMUM_BALANCE}"
     raise max_bal_err if balance > MAXIMUM_BALANCE
     @balance = balance
-    @in_journey = false
+    @journey_history = []
   end
 
   def top_up(amount)
     max_bal_error = "Cannot top up past £#{MAXIMUM_BALANCE}"
-    raise max_bal_error if (@balance + amount) > MAXIMUM_BALANCE
+    raise max_bal_error if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 
@@ -30,9 +32,12 @@ class Oystercard
     @entry_station != nil
   end
 
-  def touch_out
-    deduct(MINIMUM_FARE)
+  def touch_out(station)
+    @exit_station = station
+    journey = { 'start' => entry_station, 'end' => exit_station }
+    @journey_history << journey
     @entry_station = nil
+    deduct(MINIMUM_FARE)
   end
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -33,6 +33,8 @@ describe Oystercard do
   describe 'A group of tests that require a card to have a balance of £5 do'
   subject(:oyster) { described_class.new(5.00) }
   let(:station) { double :station }
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
 
   it 'Should create an instance (passed £5.00) with balance of £5.00' do
     expect(oyster.balance).to eq 5.00
@@ -41,16 +43,16 @@ describe Oystercard do
   describe '#deduct' do
     it 'Should deduct the minimum fare for the journey on touching out' do
       oyster.touch_in(station)
-      expect { oyster.touch_out }.to change { oyster.balance }.by(-Oystercard::MINIMUM_FARE)
-    end
-
-    it 'Should allow the user to touch in at the start of a journey' do
-      oyster.touch_in(station)
-      expect(oyster.in_journey?).to eq true
+      expect { oyster.touch_out(station) }.to change { oyster.balance }.by(-Oystercard::MINIMUM_FARE)
     end
   end
 
   describe '#touch_in' do
+    it 'Should allow the user to touch in at the start of a journey' do
+      oyster.touch_in(station)
+      expect(oyster.in_journey?).to eq true
+    end
+
     it 'Should accept and remember touch in station' do
       oyster.touch_in(station)
       expect(oyster.entry_station).to eq station
@@ -59,9 +61,20 @@ describe Oystercard do
 
   describe '#touch_out' do
     it 'Should allow a user to touch out at the end of a journey' do
-      oyster.touch_in(station)
-      oyster.touch_out
+      oyster.touch_in(entry_station)
+      oyster.touch_out(exit_station)
       expect(oyster.in_journey?).to eq false
+    end
+
+    it 'Should accept and remember touch out station' do
+      oyster.touch_out(station)
+      expect(oyster.exit_station).to eq station
+    end
+
+    it 'Should record the details of the journey on touching out' do
+      oyster.touch_in(entry_station)
+      oyster.touch_out(exit_station)
+      expect(oyster.journey_history).to include('start' => entry_station, 'end' => exit_station)
     end
   end
 end
